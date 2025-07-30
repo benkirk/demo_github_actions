b3286b73da7f01182c6b14351791fe980983e3c1
Benjamin Kirk <benkirk@ucar.edu>
2025-07-30 12:21:43 -0600

    Almalinux 10

---
+++ scripts/install_npl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#----------------------------------------------------------------------------
+# environment
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${SCRIPTDIR}/build_common.cfg || { echo "cannot locate ${SCRIPTDIR}/build_common.cfg!!"; exit 1; }
+#----------------------------------------------------------------------------
+
+set -e
+source /container/config_env.sh
+conda --version 2>/dev/null || ${SCRIPTDIR}/install_conda.sh && source /container/config_env.sh
+
+
+if [ ! -d "${STAGE_DIR}/npl" ]; then
+    git clone https://github.com/NCAR/ncar-conda.git ${STAGE_DIR}/npl
+fi
+
+cd ${STAGE_DIR}/npl
+git clean -xdf .
+
+latest_yml=$(find envs/npl/ -maxdepth 1 -name "npl*.y*ml" | sort | tail -n 1)
+
+[ -f "${latest_yml}" ] || { echo "cannot locate latest NPL YAML file!!"; exit 1; }
+
+case "$(uname -m)" in
+    *"aarch64"*)
+        sed -i 's/- libblas/#- libblas/' ${latest_yml}
+        ;;
+esac
+
+git diff
+
+conda env create -vvv --file ${latest_yml} --name npl #--prefix /container/npl/2025a
